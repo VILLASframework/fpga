8a9a729e2477c40f25fec21284b5008b66b92f89
Pascal Henry Bauer <pascal.bauer@rwth-aachen.de>
2022-11-03 05:17:20 -0400

    fpga Device formatting

--- include/villas/fpga/fpgaDevice.hpp
+++ include/villas/fpga/fpgaDevice.hpp
@@ -53,62 +53,115 @@ namespace fpga
 class FpgaDevice
 {
     public:
+        using Ptr = std::shared_ptr<FpgaDevice>;
+        using List = std::list<Ptr>;
+
         virtual ~FpgaDevice();
 
-// 	bool init();
-// 	bool stop()  { return true; }
-// 	bool check() { return true; }
-// 	bool reset() { return true; }
-// 	void dump()  { }
+        // bool init();
 
-// 	ip::Core::Ptr
-// 	lookupIp(const std::string &name) const;
+        // 	bool stop()
+        // 	{
+        // 		return true;
+        // 	}
 
-// 	ip::Core::Ptr
-// 	lookupIp(const Vlnv &vlnv) const;
+        // 	bool check()
+        // 	{
+        // 		return true;
+        // 	}
 
-// 	ip::Core::Ptr
-// 	lookupIp(const ip::IpIdentifier &id) const;
+        // 	bool reset()
+        // 	{
+        // 		// TODO: Try via sysfs?
+        // 		// echo 1 > /sys/bus/pci/devices/0000\:88\:00.0/reset
+        // 		return true;
+        // 	}
 
-// 	bool
-// 	mapMemoryBlock(const MemoryBlock &block);
+        // 	void dump()
+        // 	{ }
 
-// private:
-// 	// Cache a set of already mapped memory blocks
-// 	std::set<MemoryManager::AddressSpaceId> memoryBlocksMapped;
+        virtual ip::Core::Ptr lookupIp(const std::string &name) const;
 
-// public:	// TODO: make this private
+        ip::Core::Ptr lookupIp(const Vlnv &vlnv) const;
 
-// 	bool doReset;					// Reset VILLASfpga during startup?
+        // 	ip::Core::Ptr
+        // 	lookupIp(const ip::IpIdentifier &id) const;
 
-// 	std::string name;				// The name of the FPGA card
+        bool mapMemoryBlock(const MemoryBlock &block);
 
-// 	std::shared_ptr<kernel::pci::Device> pdev;	// PCI device handle
+        // private:
+        // 	// Cache a set of already mapped memory blocks
+        // 	std::set<MemoryManager::AddressSpaceId> memoryBlocksMapped;
 
-// 	// The VFIO container that this card is part of
-// 	std::shared_ptr<kernel::vfio::Container> vfioContainer;
+        // public:	// TODO: make this private
+        // 	ip::Core::List ips;				// IPs located
+        // on this FPGA card
 
-// 	// The VFIO device that represents this card
-// 	kernel::vfio::Device* vfioDevice;
+        // 	bool doReset;					// Reset
+        // VILLASfpga during startup?
+        int affinity; // Affinity for MSI interrupts
 
-// 	// Slave address space ID to access the PCIe address space from the FPGA
-// 	MemoryManager::AddressSpaceId addrSpaceIdDeviceToHost;
+        std::string name;
 
-// 	// Address space identifier of the master address space of this FPGA card.
-// 	// This will be used for address resolution of all IPs on this card.
-// 	MemoryManager::AddressSpaceId addrSpaceIdHostToDevice;
+        // 	std::shared_ptr<kernel::pci::Device> pdev;	// PCI device
+        // handle
 
-// protected:
-// 	Logger
-// 	getLogger() const
-// 	{ return villas::logging.get(name); }
+        // 	// The VFIO container that this card is part of
+        // 	std::shared_ptr<kernel::vfio::Container> vfioContainer;
 
-// 	Logger logger;
+        // The VFIO device that represents this card
+        kernel::vfio::Device *vfioDevice;
+
+        // 	// Slave address space ID to access the PCIe address space from
+        MemoryManager::AddressSpaceId addrSpaceIdDeviceToHost;
+
+        // 	// Address space identifier of the master address space of this
+        // FPGA card.
+        // 	// This will be used for address resolution of all IPs on this
+        MemoryManager::AddressSpaceId addrSpaceIdHostToDevice;
+
+        // protected:
+        // 	Logger
+        // 	getLogger() const
+        // 	{
+        // 		return villas::logging.get(name);
+        // 	}
+
+        // 	Logger logger;
 };
 
 class FpgaDeviceFactory : public plugin::Plugin
 {
     public:
+        static FpgaDevice::List
+        make(json_t *json,
+             std::shared_ptr<kernel::pci::DeviceList> pci,
+             std::shared_ptr<kernel::vfio::Container> vc);
+
+        // static FpgaDevice *create()
+        // {
+        //         return new FpgaDevice();
+        // }
+
+        // static Logger getStaticLogger()
+        // {
+        //         return villas::logging.get("pcie:card:factory");
+        // }
+
+        // virtual std::string getName() const
+        // {
+        //         return "pcie";
+        // }
+
+        // virtual std::string getDescription() const
+        // {
+        //         return "Xilinx PCIe FPGA cards";
+        // }
+
+        // virtual std::string getType() const
+        // {
+        //         return "card";
+        // }
 };
 }
 }
