cd836502b6353b7791959f5f6558127ab78efa0a
Pascal Henry Bauer <pascal.bauer@rwth-aachen.de>
2023-03-10 19:17:22 +0100

    removed faulty error handling

--- lib/platform_card.cpp
+++ lib/platform_card.cpp
@@ -1,9 +1,10 @@
-/** 
+/**
  * Author: Pascal Henry Bauer <pascal.bauer@rwth-aachen.de>
- * Based on the work of: Steffen Vogel <post@steffenvogel.de> and Daniel Krebs <github@daniel-krebs.net>
+ * Based on the work of: Steffen Vogel <post@steffenvogel.de> and Daniel Krebs
+ *<github@daniel-krebs.net>
  *
- * SPDX-FileCopyrightText: 2017 Institute for Automation of Complex Power Systems, EONERC
- * SPDX-License-Identifier: Apache-2.0
+ * SPDX-FileCopyrightText: 2017 Institute for Automation of Complex Power
+ *Systems, EONERC SPDX-License-Identifier: Apache-2.0
  *********************************************************************************/
 
 #include <villas/fpga/platform_card.hpp>
@@ -13,55 +14,57 @@ using namespace villas::fpga;
 
 int PlatformCard::Test()
 {
-    /* To investigate
-       -relation between vfio device and pci device
-            - vfio dev uses pci device as member
-            -> when is it accessed??
-    
-    */
+        /* To investigate
+           -relation between vfio device and pci device
+                - vfio dev uses pci device as member
+                -> when is it accessed??
 
-    /*  Report of dependencies to produce a minimal card example
+        */
 
-    Factory in pcieCard:
-        card->name = std::string(card_name);
-        card->vfioContainer = vc;
-        card->affinity = affinity;
-        card->doReset = do_reset != 0;
-        card->polling = (polling != 0);
-        card->pdev
-        {
-            pciDevice* lookupPci
+        /*  Report of dependencies to produce a minimal card example
+
+        Factory in pcieCard:
+            card->name = std::string(card_name);
+            card->vfioContainer = vc;
+            card->affinity = affinity;
+            card->doReset = do_reset != 0;
+            card->polling = (polling != 0);
+            card->pdev
             {
-                std::make_shared<Device>(id, slot)
+                pciDevice* lookupPci
+                {
+                    std::make_shared<Device>(id, slot)
+                }
             }
-        }
-        card -> init()
-        {
-            vfioDevice = vfioContainer->attachDevice(pdev);
-            { IMPORTANT two implementations exist with overloaded parameters, PCI dev and some generic
-                >>> investigation <<<
+            card -> init()
+            {
+                vfioDevice = vfioContainer->attachDevice(pdev);
+                { IMPORTANT two implementations exist with overloaded
+        parameters, PCI dev and some generic
+                    >>> investigation <<<
+                }
+                vfioDevice->pciEnable()
             }
-            vfioDevice->pciEnable()
-        }
-        card->ips = ip::CoreFactory::make(card.get(), json_ips);
-        + some messing with cores through getIp func
-    */
-   
+            card->ips = ip::CoreFactory::make(card.get(), json_ips);
+            + some messing with cores through getIp func
+        */
+
         auto vfioContainer = std::make_shared<kernel::vfio::Container>();
         // DMESG: xilinx-vdma a0000000.dma: Adding to iommu group 2
         const int IOMMU_GROUP = 2;
         auto group = std::make_unique<kernel::vfio::Group>(IOMMU_GROUP, true);
-        //vfioContainer -> attachGroup()
-        //vfioContainer -> attachDevice("xilinx-vdma a0000000.dma", IOMMU_GROUP);
-
+        // vfioContainer -> attachGroup()
+        // vfioContainer -> attachDevice("xilinx-vdma a0000000.dma",
+        // IOMMU_GROUP);
 
         return 0;
 }
 
-PlatformCard::PlatformCard(std::shared_ptr<kernel::vfio::Container> vfioContainer)
+
+PlatformCard::PlatformCard(
+    std::shared_ptr<kernel::vfio::Container> vfioContainer)
 {
-        this -> vfioContainer = vfioContainer;
+        this->vfioContainer = vfioContainer;
 
         logger = villas::logging.get("PlatformCard");
-        
 }
