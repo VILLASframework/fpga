3aaf2e93006b29bed81780c2524fcc7419094cf1
IgnoreWarnings <pascalhenrybauer@rwth-aachen.de>
2023-07-20 14:38:56 +0000

    set local var

--- lib/platform_card.cpp
+++ lib/platform_card.cpp
@@ -72,10 +72,10 @@ void PlatformCard::connect()
 
         vfioContainer->attachGroup(group);
 
-        auto device = std::make_shared<kernel::vfio::Device>(
+        this->vfioDevice = std::make_shared<kernel::vfio::Device>(
             DEV_NAME,
             group->getFileDescriptor());
-        group->attachDevice(device);
+        group->attachDevice(this->vfioDevice);
 }
 
 std::list<std::shared_ptr<PlatformCard> >
@@ -156,45 +156,45 @@ PlatformCardFactory::make(json_t *json,
 		// if (not card->check())
 		// 	throw RuntimeError("Checking of FPGA card {} failed", card_name);
 
-		// Additional static paths for AXI-Steram switch
-		if (json_paths != nullptr) {
-			if (not json_is_array(json_paths))
-				throw ConfigError(json_paths, err, "", "Switch path configuration must be an array");
+		// // Additional static paths for AXI-Steram switch
+		// if (json_paths != nullptr) {
+		// 	if (not json_is_array(json_paths))
+		// 		throw ConfigError(json_paths, err, "", "Switch path configuration must be an array");
 
-			size_t i;
-			json_t *json_path;
-			json_array_foreach(json_paths, i, json_path) {
-				const char *from, *to;
-				int reverse = 0;
+		// 	size_t i;
+		// 	json_t *json_path;
+		// 	json_array_foreach(json_paths, i, json_path) {
+		// 		const char *from, *to;
+		// 		int reverse = 0;
 
-				ret = json_unpack_ex(json_path, &err, 0, "{ s: s, s: s, s?: b }",
-					"from", &from,
-					"to", &to,
-					"reverse", &reverse
-				);
-				if (ret != 0)
-					throw ConfigError(json_path, err, "", "Cannot parse switch path config");
+		// 		ret = json_unpack_ex(json_path, &err, 0, "{ s: s, s: s, s?: b }",
+		// 			"from", &from,
+		// 			"to", &to,
+		// 			"reverse", &reverse
+		// 		);
+		// 		if (ret != 0)
+		// 			throw ConfigError(json_path, err, "", "Cannot parse switch path config");
 
-				auto masterIpCore = card->lookupIp(from);
-				if (!masterIpCore)
-					throw ConfigError(json_path, "", "Unknown IP {}", from);
+		// 		auto masterIpCore = card->lookupIp(from);
+		// 		if (!masterIpCore)
+		// 			throw ConfigError(json_path, "", "Unknown IP {}", from);
 
-				auto slaveIpCore = card->lookupIp(to);
-				if (!slaveIpCore)
-					throw ConfigError(json_path, "", "Unknown IP {}", to);
+		// 		auto slaveIpCore = card->lookupIp(to);
+		// 		if (!slaveIpCore)
+		// 			throw ConfigError(json_path, "", "Unknown IP {}", to);
 
-				auto masterIpNode = std::dynamic_pointer_cast<ip::Node>(masterIpCore);
-				if (!masterIpNode)
-					throw ConfigError(json_path, "", "IP {} is not a streaming node", from);
+		// 		auto masterIpNode = std::dynamic_pointer_cast<ip::Node>(masterIpCore);
+		// 		if (!masterIpNode)
+		// 			throw ConfigError(json_path, "", "IP {} is not a streaming node", from);
 
-				auto slaveIpNode = std::dynamic_pointer_cast<ip::Node>(slaveIpCore);
-				if (!slaveIpNode)
-					throw ConfigError(json_path, "", "IP {} is not a streaming node", to);
+		// 		auto slaveIpNode = std::dynamic_pointer_cast<ip::Node>(slaveIpCore);
+		// 		if (!slaveIpNode)
+		// 			throw ConfigError(json_path, "", "IP {} is not a streaming node", to);
 
-				if (not masterIpNode->connect(*slaveIpNode, reverse != 0))
-					throw ConfigError(json_path, "", "Failed to connect node {} to {}", from, to);
-			}
-		}
+		// 		if (not masterIpNode->connect(*slaveIpNode, reverse != 0))
+		// 			throw ConfigError(json_path, "", "Failed to connect node {} to {}", from, to);
+		// 	}
+		// }
 
 		cards.push_back(std::move(card));
 	}
