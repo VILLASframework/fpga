fd9c8d1357038494053e0fbd490fbdc15822ad74
IgnoreWarnings <pascalhenrybauer@rwth-aachen.de>
2024-02-15 13:24:58 +0000

    implement efds in platform_intc

--- lib/ips/platform_intc.cpp
+++ lib/ips/platform_intc.cpp
@@ -1,4 +1,10 @@
 #include <villas/fpga/ips/platform_intc.hpp>
+#include <villas/fpga/platform_card.hpp>
+
+bool PlatformInterruptController::init(){
+  auto platform_card = dynamic_cast<villas::fpga::PlatformCard*>(card);
+  platform_card->devices[0]->platformInit(efds);
+}
 
 bool PlatformInterruptController::enableInterrupt(
     InterruptController::IrqMaskType mask, bool polling) {
