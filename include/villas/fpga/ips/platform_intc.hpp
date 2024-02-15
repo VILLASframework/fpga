c2fad10f1aad76e872e649d68f2df2145f2e5cf6
IgnoreWarnings <pascalhenrybauer@rwth-aachen.de>
2024-02-15 13:24:02 +0000

    override intc init

--- include/villas/fpga/ips/platform_intc.hpp
+++ include/villas/fpga/ips/platform_intc.hpp
@@ -5,6 +5,8 @@
 class PlatformInterruptController
     : public villas::fpga::ip::InterruptController {
 public:
+  bool init() override;
+
   bool enableInterrupt(InterruptController::IrqMaskType mask,
                        bool polling) override;
   bool enableInterrupt(IrqPort irq, bool polling) override;
