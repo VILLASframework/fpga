a0c97d4e775c0480b3c8cf06a908b425fb0b12ed
IgnoreWarnings <pascalhenrybauer@rwth-aachen.de>
2024-02-22 13:33:41 +0000

    removed dma hardcoded position

--- lib/ips/platform_intc.cpp
+++ lib/ips/platform_intc.cpp
@@ -1,9 +1,24 @@
+#include <exception>
+#include <memory>
+#include <stdexcept>
 #include <villas/fpga/ips/platform_intc.hpp>
 #include <villas/fpga/platform_card.hpp>
 
 bool PlatformInterruptController::init(){
   auto platform_card = dynamic_cast<villas::fpga::PlatformCard*>(card);
-  platform_card->devices[0]->platformInit(efds);
+
+  std::shared_ptr<villas::kernel::vfio::Device> dma_vfio_device;
+  for (auto device : platform_card->devices)
+  {
+    if (device->getName().find("dma") != std::string::npos){
+      dma_vfio_device = device;
+      break;
+    }
+  }
+
+  if(dma_vfio_device == nullptr) throw std::logic_error("No vfio device with name containing dma found.");
+
+  dma_vfio_device->platformInit(efds);
 }
 
 bool PlatformInterruptController::enableInterrupt(
